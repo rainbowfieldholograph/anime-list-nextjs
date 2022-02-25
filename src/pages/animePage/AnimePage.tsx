fcdf31505399c947009c5fd5bb860c099d308646
rainbowfieldholograph <somebleed@tutanota.com>
2022-02-25 20:21:08 +0900

    animepage update

--- src/pages/animePage/AnimePage.tsx
+++ src/pages/animePage/AnimePage.tsx
@@ -21,42 +21,46 @@ export const AnimePage: FC = ({}: AnimePageProps): JSX.Element => {
   if (!animeData) return <h1>loading...</h1>
 
   return (
-    <>
+    <div className={styles.wrapper}>
       <h1>{animeData.title}</h1>
-      <img src={animeData.images.webp.image_url} alt="" />
-      <section>
-        <h2>Information</h2>
-        <p>Type: {animeData.type}</p>
-        <p>Episodes: {animeData.episodes ? animeData.episodes : 'unknown'}</p>
-        <p>Status: {animeData.status}</p>
-        <span className={styles.genres}>
-          Genres:
-          {animeData.genres.map((genre) => (
-            <span key={genre.mal_id} className={styles.genre}>
-              {genre.name}
-            </span>
-          ))}
-        </span>
-        <p>Rating: {animeData.rating}</p>
-        <p>
-          Licensors:{' '}
-          {animeData.licensors.length > 0
-            ? animeData.licensors.map((licensor) => (
-                <span key={licensor.mal_id}>{licensor.name}</span>
-              ))
-            : 'unkonown'}
-        </p>
-        <span className={styles.genres}>
-          Synonyms:{' '}
-          {animeData.title_synonyms.map((alterTitle) => (
-            <span className={styles.alterTitle}>{alterTitle}</span>
-          ))}
-        </span>
-      </section>
-      <section>
-        <h2>Description</h2>
-        <p>{animeData.synopsis}</p>
-      </section>
-    </>
+      <div className={styles.inner}>
+        <img className={styles.poster} src={animeData.images.webp.image_url} alt="" />
+        <div className={styles.information}>
+          <h2>Information</h2>
+          <p>Type: {animeData.type}</p>
+          <p>Episodes: {animeData.episodes ? animeData.episodes : 'unknown'}</p>
+          <p>Status: {animeData.status}</p>
+          <span className={styles.genres}>
+            Genres:
+            {animeData.genres.map((genre) => (
+              <span key={genre.mal_id} className={styles.genre}>
+                {genre.name}
+              </span>
+            ))}
+          </span>
+          <p>Rating: {animeData.rating}</p>
+          <p>
+            Licensors:{' '}
+            {animeData.licensors.length > 0
+              ? animeData.licensors.map((licensor) => (
+                  <span key={licensor.mal_id}>{licensor.name}</span>
+                ))
+              : 'unkonown'}
+          </p>
+          <span className={styles.genres}>
+            Synonyms:{' '}
+            {animeData.title_synonyms.map((alterTitle) => (
+              <span className={styles.alterTitle}>{alterTitle}</span>
+            ))}
+          </span>
+        </div>
+        <div>Rating</div>
+        <aside className={styles.menu}>hello</aside>
+        <div className={styles.description}>
+          <h2>Description</h2>
+          <p>{animeData.synopsis}</p>
+        </div>
+      </div>
+    </div>
   )
 }
